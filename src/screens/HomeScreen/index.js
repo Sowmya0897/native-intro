7e370e8ccf52df5e752bb3547faa9e69310bf0fd
Sowmya Javvadi <j.l.s.sowmya333@gmail.com>
2024-07-20 20:51:28 -0500

    Change HomeScreen Description

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -27,17 +27,23 @@ const index = () => {
     <View>
       <Text style={{ fontSize: 36, alignSelf: 'center' }}> </Text>
       <Text style={{ fontSize: 36, alignSelf: 'center' }}> Welcome Home </Text>
+      <Text style={{ fontSize: 20, alignSelf: 'center' }}> </Text>
+      <Text style={{fontWeight: 'bold'}}> Give values and expression to do simple math operation </Text>
+      <Text> Examples: sin(90 deg), 4 / (1 + 3), 5*8, 9-3</Text>
+      <Text style={{ fontSize: 10, alignSelf: 'center' }}> </Text>
       <TextInput
         style={{
           height: 40,
           borderColor: 'gray',
           borderWidth: 1,
+          padding:5,
           paddingHorizontal: 10,
         }}
-        placeholder="Enter an expression"
+        placeholder="5*3*8"
         value={inputValue}
         onChangeText={(text) => setInputValue(text)}
       />
+      <Text style={{ fontSize: 24, alignSelf: 'center' }}> </Text>
       <Button title="Submit" onPress={handlePress} />
       <Text style={{ fontSize: 24, textAlign: 'center', marginTop: 20 }}>
         {result}
