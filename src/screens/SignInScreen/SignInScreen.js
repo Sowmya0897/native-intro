6659a91fde450a88c87096df90685c4c874afc3c
AL1075448_totalen <AL1075448@admin.hubtotal.net>
2024-07-30 10:28:17 +0530

    fix: temp storage in local

--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -1,80 +1,109 @@
-import { Text, View, Image, StyleSheet, useWindowDimensions, ScrollView } from 'react-native';
-import Logo from '../../../assets/pic.jpg';
-import React, {useState} from 'react'
-import CustomInput from '../../components/CustomInput';
-import CustomButton from '../../components/CustomInput/CustomButton';
-import SocialSignInButtons from '../../components/SocialSignInButtons';
-import { useNavigation } from '@react-navigation/native';
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  useWindowDimensions,
+  ScrollView,
+} from "react-native";
+import Logo from "../../../assets/pic.jpg";
+import React, { useEffect, useState } from "react";
+import CustomInput from "../../components/CustomInput";
+import CustomButton from "../../components/CustomInput/CustomButton";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import SocialSignInButtons from "../../components/SocialSignInButtons";
+import { useNavigation } from "@react-navigation/native";
 
 const SignInScreen = () => {
-    const[username, setUsername]= useState('');
-    const[password, setPassword]= useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [checkUsername, setCheckUsername] = useState('');
+  const [checkPassword, setCheckPassword] = useState('');
 
-    const {height} = useWindowDimensions();
-    const navigation = useNavigation();
+  const { height } = useWindowDimensions();
+  const navigation = useNavigation();
 
-    const onSignInPressed = () => {
-        console.warn("Sign In");
-        //validate user
-        navigation.navigate('Home');
+  const getData = async () => {
+    try {
+      const userName = await AsyncStorage.getItem('userName');
+      if (userName !== '') {
+       setCheckUsername(userName)
+      }
+      const password = await AsyncStorage.getItem('password');
+      if (password !== '') {
+       setCheckPassword(password);
+      }
+    } catch (e) {
+      console.log()
     }
-    const onForgotPasswordPressed = () => {
-        navigation.navigate('ForgotPassword');
+  };
+
+  useEffect(() => { getData() }, []);
+  
+  const onSignInPressed = () => {
+    console.warn("Sign In");
+    //validate user
+    console.log(username, password, checkUsername, checkPassword);
+    if (username === checkUsername && password === checkPassword) {
+      navigation.navigate("Home");
     }
-    const onSignUpPressed = () => {
-        navigation.navigate('SignUp');
-    }
+  };
+  const onForgotPasswordPressed = () => {
+    navigation.navigate("ForgotPassword");
+  };
+  const onSignUpPressed = () => {
+    navigation.navigate("SignUp");
+  };
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-    <View style={styles.root}>
-      <Image   
-        source={Logo} 
-        style={[styles.logo, {height: height * 0.3}]} 
-        resizeMode="contain" 
-      />
+      <View style={styles.root}>
+        <Image
+          source={Logo}
+          style={[styles.logo, { height: height * 0.3 }]}
+          resizeMode="contain"
+        />
 
-      <CustomInput 
-      placeholder="Username" 
-      value={username} 
-      setValue={setUsername}
-      />
-      <CustomInput
-       placeholder="Password" 
-       value={password} 
-       setValue={setPassword}
-       secureTextEntry
-       />
-       <CustomButton text="Sign In" onPress={onSignInPressed}/>
-       <CustomButton
-         text="Forgot Password?" 
-         onPress={onForgotPasswordPressed} 
-         type="TERTIARY"/>
-        
-        <SocialSignInButtons/>
+        <CustomInput
+          placeholder="Username"
+          value={username}
+          setValue={setUsername}
+        />
+        <CustomInput
+          placeholder="Password"
+          value={password}
+          setValue={setPassword}
+          secureTextEntry
+        />
+        <CustomButton text="Sign In" onPress={onSignInPressed} />
+        <CustomButton
+          text="Forgot Password?"
+          onPress={onForgotPasswordPressed}
+          type="TERTIARY"
+        />
 
-       <CustomButton
-         text="Don't have an account? Create one" 
-         onPress={onSignUpPressed} 
-         type="TERTIARY"/>
+        <SocialSignInButtons />
 
-    </View>
+        <CustomButton
+          text="Don't have an account? Create one"
+          onPress={onSignUpPressed}
+          type="TERTIARY"
+        />
+      </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-    root: {
-        alignItems: 'center',
-        padding: 20,
-    },
-logo:{
-    width: '80%',
+  root: {
+    alignItems: "center",
+    padding: 20,
+  },
+  logo: {
+    width: "80%",
     maxWidth: 300,
     maxHeight: 200,
-},
-
+  },
 });
 
-export default SignInScreen
-
+export default SignInScreen;
--- src/screens/SignUpScreen/SignUpScreen.js
+++ src/screens/SignUpScreen/SignUpScreen.js
@@ -1,110 +1,120 @@
-import { Text, View, StyleSheet, ScrollView } from 'react-native';
-import React, {useState} from 'react'
-import CustomInput from '../../components/CustomInput';
-import CustomButton from '../../components/CustomInput/CustomButton';
-import SocialSignInButtons from '../../components/SocialSignInButtons';
-import { useNavigation } from '@react-navigation/native';
+import { Text, View, StyleSheet, ScrollView } from "react-native";
+import React, { useState } from "react";
+import CustomInput from "../../components/CustomInput";
+import CustomButton from "../../components/CustomInput/CustomButton";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import SocialSignInButtons from "../../components/SocialSignInButtons";
+import { useNavigation } from "@react-navigation/native";
 
 const SignUpScreen = () => {
-    const[username, setUsername]= useState('');
-    const[email, setEmail]= useState('');
-    const[password, setPassword]= useState('');
-    const[passwordRepeat, setPasswordRepeat]= useState('');
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordRepeat, setPasswordRepeat] = useState("");
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const onRegisterPressed = () => {
-        console.warn("Successfully Registered");
-        navigation.navigate('ConfirmEmail');
+  const storeData = async (name,value) => {
+    try {
+      await AsyncStorage.setItem(name, value);
+    } catch (e) {
+      console.log(e)
     }
-    const onSignInPressed = () => {
-      navigation.navigate('SignIn');
-  }
-    const onTermsofUsePressed = () => {
-        console.warn("Clicked Terms of Use");
-    }
-    const onPrivacyPolicyePressed = () => {
-      console.warn("Clicked Privacy Policy");
-  }
-   
-   
+  };
+
+  const onRegisterPressed = () => {
+    storeData("userName", userName);
+    storeData("password", password);
+    
+    console.warn("Successfully Registered");
+    // navigation.navigate("ConfirmEmail");
+    navigation.navigate("SignIn");
+  };
+  const onSignInPressed = () => {
+    navigation.navigate("SignIn");
+  };
+  const onTermsofUsePressed = () => {
+    console.warn("Clicked Terms of Use");
+  };
+  const onPrivacyPolicyePressed = () => {
+    console.warn("Clicked Privacy Policy");
+  };
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-    <View style={styles.root}>
-      <Text style={styles.title}>Create an account</Text>
+      <View style={styles.root}>
+        <Text style={styles.title}>Create an account</Text>
 
-      <CustomInput 
-      placeholder="Username" 
-      value={username} 
-      setValue={setUsername}
-      />
+        <CustomInput
+          placeholder="UserName"
+          value={userName}
+          setValue={setUserName}
+        />
 
-      <CustomInput
-       placeholder="Email" 
-       value={email} 
-       setValue={setEmail}
-       />
+        <CustomInput placeholder="Email" value={email} setValue={setEmail} />
 
-      <CustomInput
-       placeholder="Password" 
-       value={password} 
-       setValue={setPassword}
-       secureTextEntry
-       />
+        <CustomInput
+          placeholder="Password"
+          value={password}
+          setValue={setPassword}
+          secureTextEntry
+        />
 
-       <CustomInput
-       placeholder="Repeat Password" 
-       value={passwordRepeat} 
-       setValue={setPasswordRepeat}
-       secureTextEntry
-       />
-       
-       <CustomButton text="Register" onPress={onRegisterPressed}/>
+        <CustomInput
+          placeholder="Repeat Password"
+          value={passwordRepeat}
+          setValue={setPasswordRepeat}
+          secureTextEntry
+        />
 
-       <Text style={styles.text}>
-        By registering, you confirm that you accept our{' '}
-        <Text style={styles.link} onPress={onTermsofUsePressed}>Terms of Use </Text> and{' '}
-        <Text style={styles.link} onPress={onPrivacyPolicyePressed} >Privacy Policy </Text>
+        <CustomButton text="Register" onPress={onRegisterPressed} />
+
+        <Text style={styles.text}>
+          By registering, you confirm that you accept our{" "}
+          <Text style={styles.link} onPress={onTermsofUsePressed}>
+            Terms of Use{" "}
+          </Text>{" "}
+          and{" "}
+          <Text style={styles.link} onPress={onPrivacyPolicyePressed}>
+            Privacy Policy{" "}
+          </Text>
         </Text>
-       
-        <SocialSignInButtons/>
 
-       <CustomButton
-         text="Have an account? Sign in" 
-         onPress={onSignInPressed} 
-         type="TERTIARY"/>
+        <SocialSignInButtons />
 
-    </View>
+        <CustomButton
+          text="Have an account? Sign in"
+          onPress={onSignInPressed}
+          type="TERTIARY"
+        />
+      </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-    root: {
-        alignItems: 'center',
-        padding: 20,
-    },
-    logo:{
-    width: '70%',
+  root: {
+    alignItems: "center",
+    padding: 20,
+  },
+  logo: {
+    width: "70%",
     maxWidth: 300,
     maxHeight: 200,
-},
-title: {
-  fontsize:36,
-  fontWeight:'bold',
-  color: '#051C60',
-  margin: 20
-},
-text:{
-  color: 'gray',
-  marginVertical: 10,
-},
-link:{
-  color:'#FDB075',
-}
-
+  },
+  title: {
+    fontsize: 36,
+    fontWeight: "bold",
+    color: "#051C60",
+    margin: 20,
+  },
+  text: {
+    color: "gray",
+    marginVertical: 10,
+  },
+  link: {
+    color: "#FDB075",
+  },
 });
 
-export default SignUpScreen
-
+export default SignUpScreen;
