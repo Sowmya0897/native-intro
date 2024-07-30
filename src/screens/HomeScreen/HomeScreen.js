306b24d09fa4dcad17b3b0e1ecedd15ae6ae2b0d
Sowmya Javvadi <j.l.s.sowmya333@gmail.com>
2024-07-30 12:54:14 -0500

    fix: local storage

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import CustomButton from "../../components/CustomInput/CustomButton";
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
+
+const HomeScreen = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [result, setResult] = useState("");
+  const navigation = useNavigation();
+
+  const onSignOutPressed = () => {
+    navigation.navigate("SignIn");
+  };
+
+  const encodedTextingValue = encodeURIComponent(inputValue);
+  const handlePress = async () => {
+    try {
+      const encodedTextingValue = encodeURIComponent(inputValue);
+      const response = await axios.get(
+        `http://api.mathjs.org/v4/?expr=${encodedTextingValue}`
+      );
+      setResult(response.data);
+    } catch (error) {
+      setResult("Error: " + error.message);
+    }
+  };
+
+  return (
+    <View>
+      <Text style={{ fontSize: 36, alignSelf: "center" }}> </Text>
+      <Text style={{ fontSize: 36, alignSelf: "center" }}> Welcome Home </Text>
+      <Text style={{ fontSize: 20, alignSelf: "center" }}> </Text>
+      <Text style={{ fontWeight: "bold" }}>
+        {" "}
+        Give values and expression to do simple math operation{" "}
+      </Text>
+      <Text> Examples: sin(90 deg), 4 / (1 + 3), 5*8, 9-3</Text>
+      <Text style={{ fontSize: 10, alignSelf: "center" }}> </Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          padding: 5,
+          paddingHorizontal: 10,
+        }}
+        placeholder="5*3*8"
+        value={inputValue}
+        onChangeText={(text) => setInputValue(text)}
+      />
+      <Text style={{ fontSize: 24, alignSelf: "center" }}> </Text>
+      <Button title="Submit" onPress={handlePress} />
+      <Text style={{ fontSize: 24, textAlign: "center", marginTop: 20 }}>
+        {result}
+      </Text>
+
+      <CustomButton
+        text="Sign out"
+        onPress={onSignOutPressed}
+        type="TERTIARY"
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -1,80 +1,91 @@
-import { Text, View, Image, StyleSheet, useWindowDimensions, ScrollView } from 'react-native';
-import Logo from '../../../assets/pic.jpg';
-import React, {useState} from 'react'
-import CustomInput from '../../components/CustomInput';
-import CustomButton from '../../components/CustomInput/CustomButton';
-import SocialSignInButtons from '../../components/SocialSignInButtons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  useWindowDimensions,
+  View
+} from "react-native";
+import Logo from "../../../assets/pic.jpg";
+import CustomInput from "../../components/CustomInput";
+import CustomButton from "../../components/CustomInput/CustomButton";
+import SocialSignInButtons from "../../components/SocialSignInButtons";
 
-const SignInScreen = () => {
-    const[username, setUsername]= useState('');
-    const[password, setPassword]= useState('');
+const SignInScreen = ({ route }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const userName = route.params?.userNames;
+  const passwords = route.params?.passwords;
 
-    const {height} = useWindowDimensions();
-    const navigation = useNavigation();
+  const { height } = useWindowDimensions();
+  const navigation = useNavigation();
 
-    const onSignInPressed = () => {
-        console.warn("Sign In");
-        //validate user
-        navigation.navigate('Home');
+
+  const onSignInPressed = () => {
+    console.warn("Sign In");
+    console.log(username, password, userName, passwords);
+    //validate user
+    if (username === userName && password === passwords) {
+      navigation.navigate("Home");
     }
-    const onForgotPasswordPressed = () => {
-        navigation.navigate('ForgotPassword');
-    }
-    const onSignUpPressed = () => {
-        navigation.navigate('SignUp');
-    }
+  };
+  const onForgotPasswordPressed = () => {
+    navigation.navigate("ForgotPassword");
+  };
+  const onSignUpPressed = () => {
+    navigation.navigate("SignUp");
+  };
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-    <View style={styles.root}>
-      <Image   
-        source={Logo} 
-        style={[styles.logo, {height: height * 0.3}]} 
-        resizeMode="contain" 
-      />
+      <View style={styles.root}>
+        <Image
+          source={Logo}
+          style={[styles.logo, { height: height * 0.3 }]}
+          resizeMode="contain"
+        />
 
-      <CustomInput 
-      placeholder="Username" 
-      value={username} 
-      setValue={setUsername}
-      />
-      <CustomInput
-       placeholder="Password" 
-       value={password} 
-       setValue={setPassword}
-       secureTextEntry
-       />
-       <CustomButton text="Sign In" onPress={onSignInPressed}/>
-       <CustomButton
-         text="Forgot Password?" 
-         onPress={onForgotPasswordPressed} 
-         type="TERTIARY"/>
-        
-        <SocialSignInButtons/>
+        <CustomInput
+          placeholder="Username"
+          value={username}
+          setValue={setUsername}
+        />
+        <CustomInput
+          placeholder="Password"
+          value={password}
+          setValue={setPassword}
+          secureTextEntry
+        />
+        <CustomButton text="Sign In" onPress={onSignInPressed} />
+        <CustomButton
+          text="Forgot Password?"
+          onPress={onForgotPasswordPressed}
+          type="TERTIARY"
+        />
 
-       <CustomButton
-         text="Don't have an account? Create one" 
-         onPress={onSignUpPressed} 
-         type="TERTIARY"/>
+        <SocialSignInButtons />
 
-    </View>
+        <CustomButton
+          text="Don't have an account? Create one"
+          onPress={onSignUpPressed}
+          type="TERTIARY"
+        />
+      </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-    root: {
-        alignItems: 'center',
-        padding: 20,
-    },
-logo:{
-    width: '80%',
+  root: {
+    alignItems: "center",
+    padding: 20,
+  },
+  logo: {
+    width: "80%",
     maxWidth: 300,
     maxHeight: 200,
-},
-
+  },
 });
 
-export default SignInScreen
-
+export default SignInScreen;
